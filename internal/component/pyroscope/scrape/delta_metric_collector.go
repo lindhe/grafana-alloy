648fb0c54455041cd1d8d3067f4e6c4f0d297448
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2025-04-18 16:17:05 +0700

    update to include container name in the metric

--- internal/component/pyroscope/scrape/delta_metric_collector.go
+++ internal/component/pyroscope/scrape/delta_metric_collector.go
@@ -1,20 +1,19 @@
 package scrape
 
 import (
+	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 type deltaMetricCollector struct {
 	manager *Manager
 	desc    *prometheus.Desc
-	label   string
 }
 
 func newDeltaMetricCollector(manager *Manager) *deltaMetricCollector {
 	return &deltaMetricCollector{
 		manager: manager,
-		desc:    prometheus.NewDesc("pyroscope_delta_map_size", "Size of the DeltaMap", []string{"aggregation"}, nil),
-		label:   "namespace",
+		desc:    prometheus.NewDesc("pyroscope_delta_map_size", "Size of the DeltaMap", []string{"namespace", "controller"}, nil),
 	}
 }
 
@@ -26,21 +25,31 @@ func (d *deltaMetricCollector) Collect(metrics chan<- prometheus.Metric) {
 	m := d.manager
 	m.mtxScrape.Lock()
 	defer m.mtxScrape.Unlock()
+	type aggregationKey struct {
+		ns         string
+		controller string
+	}
 
-	aggregation := make(map[string]int64)
+	aggregation := make(map[aggregationKey]int64)
 	for _, sp := range m.targetsGroups {
 		loops := sp.activeScrapeLoops()
 		for _, loop := range loops {
 			if da, ok := loop.appender.(*deltaAppender); ok {
 				size := da.deltaMapSize()
-				k := loop.Target.allLabels.Get(d.label)
-				aggregation[k] += int64(size)
+				ns := loop.Target.allLabels.Get("namespace")
+				controller := fmt.Sprintf("%s-%s-%s",
+					loop.Target.allLabels.Get("container"),
+					loop.Target.allLabels.Get("__meta_kubernetes_pod_container_name"),
+					loop.Target.allLabels.Get("__meta_kubernetes_pod_controller_name"),
+				)
+				a := aggregationKey{ns, controller}
+				aggregation[a] += int64(size)
 			}
 		}
 	}
 
-	for namespace, size := range aggregation {
-		metric, err := prometheus.NewConstMetric(d.desc, prometheus.GaugeValue, float64(size), namespace)
+	for key, size := range aggregation {
+		metric, err := prometheus.NewConstMetric(d.desc, prometheus.GaugeValue, float64(size), key.ns, key.controller)
 		if err != nil {
 			continue
 		}
